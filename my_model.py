23a3fef92c4ef7078203b2db36696d74136f8bcc
SHx3 <ksh60706@gmail.com>
2019-08-12 00:50:04 +0900

    변경파일

---
+++ my_model.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class Baseline(nn.Module):
+    def __init__(self, hidden_size, out_size):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=hidden_size, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(hidden_size),
+            nn.Conv2d(hidden_size, hidden_size, 3, 2, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(hidden_size),
+            nn.Conv2d(hidden_size, hidden_size, 3, 2, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(hidden_size),
+            nn.Conv2d(hidden_size, hidden_size, 3, 2, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(hidden_size),
+            nn.Conv2d(hidden_size, hidden_size, 3, 2, 1),
+            nn.ReLU(True),
+            nn.BatchNorm2d(hidden_size),
+            nn.Conv2d(hidden_size, out_size, 4, 1),
+        )
+
+    def forward(self, image):
+        return self.net(image).squeeze(-1).squeeze(-1)
+
+
+class Resnet(nn.Module):
+    def __init__(self, out_size):
+        super().__init__()
+        model = models.resnet18(pretrained=True)
+        model = list(model.children())[:-1]
+        model.append(nn.Conv2d(512, out_size, 1))
+        self.net = nn.Sequential(*model)
+
+    def forward(self, image):
+        return self.net(image).squeeze(-1).squeeze(-1)
+
+class VGG(nn.Module):
+    def __init__(self):
+        super(VGG, self).__init__()
+        self.cnn = nn.Sequential(
+            # 3 x 128 x 128
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # 64 x 64 x 64
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # 128 x 32 x 32
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # 256 x 16 x 16
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+
+            # 512 x 8 x 8
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+
+            # 512 x 4 x 4
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(512 * 4 * 4, 4096),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(4096, 2048),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+            nn.Linear(2048, 30)
+        )
+
+    def forward(self, x):
+        output = self.cnn(x)
+        output = output.view(output.size(0), -1)
+        output = self.fc(output)
+        return output
